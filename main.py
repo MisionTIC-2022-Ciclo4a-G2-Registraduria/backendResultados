5301a536f71d8b68b028bfa022621e0c16559f2b
Syaga09 <111614389+syaga2409@users.noreply.github.com>
2022-11-04 11:05:18 -0500

    Sprint 1

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import json
 
 from flask import Flask
 from flask import jsonify
-from flask import request
 from flask_cors import CORS
 from waitress import serve
 
-from blueprints.candidateBlueprints import candidate_blueprints
-from blueprints.politicalPartyBlueprints import political_party_blueprints
-from blueprints.voteBlueprints import vote_blueprints
-from blueprints.tableBlueprints import table_blueprints
-
+from blueprints.candidate_blueprints import candidate_blueprints
+from blueprints.political_party_blueprints import political_party_blueprints
+from blueprints.vote_blueprints import vote_blueprints
+from blueprints.table_blueprints import table_blueprints
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -22,20 +20,18 @@ app.register_blueprint(table_blueprints)
 
 @app.route("/", methods=['GET'])
 def home():
-    response = {"message": "Welcome to the Results Services"}
-
+    response = {"message": "Welcome to the Results microservices"}
     return jsonify(response)
 
 
-# Config and Execute App
+# Config and execute app
 def load_file_config():
     with open("config.json") as file:
         data = json.load(file)
-
     return data
 
 
 if __name__ == '__main__':
     data_config = load_file_config()
-    print("Server running: http://" + data_config.get('url-backend') + ":" + str(data_config.get('port')))
+    print("Server running: http://"+data_config.get('url-backend')+":"+str(data_config.get('port')))
     serve(app, host=data_config.get('url-backend'), port=data_config.get('port'))
