2d4b14cedf1263dbdc5891c3bbf687135baf2525
Syaga09 <111614389+syaga2409@users.noreply.github.com>
2022-11-13 20:30:17 -0500

    cambios models

---
+++ models/abstract_model.py
@@ -0,0 +1,13 @@
+from abc import ABCMeta
+
+
+class AbstractModel(metaclass=ABCMeta):
+
+    def __init__(self, data: dict):
+        """
+        This constructor is used to transform a dictionary called data
+        into an object making a flexible attribute definition
+        :param data: data to convert into object
+        """
+        for key, value in data.items():
+            setattr(self, key, value)
